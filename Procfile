a1ced00a7b15dd2e1997acedbb1b83140118eb68
Addison Grillo <addisongrillo@gmail.com>
2019-08-03 12:54:56 -0400

    heroku try 1

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn TheWallBackend.wsgi --log-file -
---
+++ src/containers/PostListView.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Posts from "../components/Posts"
+import axios from 'axios'
+
+class PostList extends React.Component {
+    state = {
+        posts: []
+    }
+    update = () => {
+        setTimeout(this.updateAfterTimeout, 100)
+    }
+
+    updateAfterTimeout = () =>{
+        axios.get('https://thewall2019.herokuapp.com/posts/')
+        .then(res=> {
+            this.setState({
+                posts: res.data
+            })
+            console.log(this.state.posts)
+        })
+    }
+
+    componentDidMount(){
+        axios.get('https://thewall2019.herokuapp.com/posts/')
+        .then(res=> {
+            this.setState({
+                posts: res.data
+            })
+            console.log(this.state.posts)
+        })
+    }
+    render(){
+        return(
+            <Posts update={this.update.bind(this)} data={this.state.posts}/>
+        )
+    }
+}
+
+export default PostList
